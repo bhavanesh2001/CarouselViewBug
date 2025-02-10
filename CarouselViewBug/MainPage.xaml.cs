a83d41d03d28258c24af54c65bc43943d6342b2e
bhavanesh001 <bhavanesh001@gmail.com>
2025-02-10 18:25:27 +0530

    Add project files.

---
+++ CarouselViewBug/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Reflection;
+
+namespace CarouselViewBug
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+           
+            
+        }
+
+     
+
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var pickMedia = new PickOptions
+            {
+                PickerTitle = "",
+                FileTypes = FilePickerFileType.Jpeg
+            };
+            var res = await FilePicker.PickMultipleAsync(pickMedia);
+            caroselView.ItemsSource = res;
+        }
+       
+    }
+
+}
